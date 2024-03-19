7bd9768009aad993cc17d82cc809d4a001031420
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-03-19 23:22:02 +0100

    Updated shaderc script

--- ext/update_shaderc_sources.py
+++ ext/update_shaderc_sources.py
@@ -17,15 +17,17 @@ repositories."""
 from operator import attrgetter
 import argparse
 import json
-import distutils.dir_util
+import os
 import os.path
 import subprocess
 import sys
+
 KNOWN_GOOD_FILE = 'known_good.json'
 # Maps a site name to its hostname.
 SITE_TO_HOST = {'github': 'github.com',
                 'gitlab': 'gitlab.com'}
 VERBOSE = True
+
 def command_output(cmd, directory, fail_ok=False):
     """Runs a command in a directory and returns its standard output stream.
     Captures the standard error stream.
@@ -42,6 +44,7 @@ def command_output(cmd, directory, fail_ok=False):
     if VERBOSE:
         print(stdout)
     return stdout
+
 class GoodCommit(object):
     """Represents a good commit for a repository."""
     def __init__(self, json):
@@ -55,6 +58,7 @@ class GoodCommit(object):
         self.subrepo = json['subrepo']
         self.subdir = json['subdir'] if ('subdir' in json) else '.'
         self.commit = json['commit']
+
     def GetUrl(self, style='https'):
         """Returns the URL for the repository."""
         host = SITE_TO_HOST[self.site]
@@ -64,21 +68,25 @@ class GoodCommit(object):
             host=host,
             sep=sep,
             subrepo=self.subrepo)
+    
     def AddRemote(self):
         """Add the remote 'known-good' if it does not exist."""
         if len(command_output(['git', 'remote', 'get-url', 'known-good'], self.subdir, fail_ok=True)) == 0:
             command_output(
                 ['git', 'remote', 'add', 'known-good', self.GetUrl()], self.subdir)
+            
     def HasCommit(self):
         """Check if the repository contains the known-good commit."""
         return 0 == subprocess.call(['git', 'rev-parse', '--verify', '--quiet',
                                      self.commit + '^{commit}'],
                                     cwd=self.subdir)
     def Clone(self):
-        distutils.dir_util.mkpath(self.subdir)
+        os.makedirs(self.subdir, exist_ok=True)
         command_output(['git', 'clone', self.GetUrl(), '.'], self.subdir)
+
     def Fetch(self):
         command_output(['git', 'fetch', 'known-good'], self.subdir)
+
     def Checkout(self):
         if not os.path.exists(os.path.join(self.subdir, '.git')):
             self.Clone()
@@ -86,10 +94,12 @@ class GoodCommit(object):
         if not self.HasCommit():
             self.Fetch()
         command_output(['git', 'checkout', self.commit], self.subdir)
+
 def GetGoodCommits(known_good_file):
     """Returns the latest list of GoodCommit objects."""
     with open(known_good_file) as known_good:
         return [GoodCommit(c) for c in json.loads(known_good.read())['commits']]
+    
 def main():
     parser = argparse.ArgumentParser(
         description='Get Shaderc source dependencies at a known-good commit')
@@ -99,7 +109,7 @@ def main():
                         help="The file containing known-good commits. Default is \'' + KNOWN_GOOD_FILE + '\'.")
     args = parser.parse_args()
     commits = GetGoodCommits(args.known_good_file)
-    distutils.dir_util.mkpath(args.dir)
+    os.makedirs(args.dir, exist_ok=True)
     print('Change directory to {d}'.format(d=args.dir))
     os.chdir(args.dir)
     # Create the subdirectories in sorted order so that parent git repositories
@@ -108,5 +118,6 @@ def main():
         print('Get {n}\n'.format(n=c.name))
         c.Checkout()
     sys.exit(0)
+    
 if __name__ == '__main__':
     main()
